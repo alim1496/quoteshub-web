56abab2be8c28441af8a3ddc8bccbae770d35cbf
alim1496 <alim1496@gmail.com>
2021-01-17 00:00:32 +0600

    update use effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Footer from "../components/Footer";
 import QuoteList from "../components/QuoteList";
 import TopicsBar from "../components/TopicsBar";
 import "../style/home.scss";
 
-const fetchHomeData = () => {};
-
-const Home = () => {
+const Home = ({ location, match }) => {
   const partitions = [];
   const [topics, setTopics] = useState([]);
   const [quotes, setQuotes] = useState([]);
+  const { pathname } = location;
 
   useEffect(() => {
+    checkPath(pathname);
+  }, [pathname]);
+
+  const checkPath = (path) => {
+    if (path.includes("category")) {
+      const { id, name } = match.params;
+      fetchCategory(id);
+    } else {
+      fetchHome();
+    }
+  };
+
+  const fetchCategory = (id) => {
+    fetch(
+      `http://quotes-ocean.herokuapp.com/api/quotes/v2/category/${id}?page=1&size=30`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setQuotes(result);
+      }, (error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchHome = () => {
     fetch(
       "http://quotes-ocean.herokuapp.com/api/quotes/v3/home/?featured=true&with_topics=true&page=1&size=30"
     )
@@ -22,7 +46,7 @@ const Home = () => {
       }, (error) => {
         console.log(error);
       });
-  }, []);
+  };
 
   if (quotes.length > 0) {
     const partitionLength = Math.ceil(quotes.length / 3);
